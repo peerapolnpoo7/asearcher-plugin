177f89010650af8a1cb766e4777e89b1002d9016
Ol2iGiN <pongthep.dk@gmail.com>
2018-08-10 16:31:23 +0700

    commit

--- candidatecustommer/assets/js/cv-family.js
+++ candidatecustommer/assets/js/cv-family.js
@@ -35,7 +35,6 @@ $(document).ready(function(){
 					$("#boxChildren").find('.row:last-child').clone().appendTo("#boxChildren");
 					$("#boxChildren").find('.row:last-child label:contains("{=id=}")').text(i+".คำนำหน้าชื่อ");
 				}
-				// $("#boxChildren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
 			}
 		}
 	}).on("keyup","#NumBrethren",function(){
@@ -44,43 +43,16 @@ $(document).ready(function(){
 			$("#boxBrethren").find('.row:first-child').addClass('hidden');
 		}else{
 			$("#boxBrethren").find('.row:first-child').removeClass('hidden');
-			if(this.value > 1){
-
-
-				// for(var i=1;i<this.value;i++){
-				// 	console.log(i);
-				// 	$("#boxBrethren").find('.row:last-child' ).clone().appendTo("#boxBrethren");
-				// 	$("#boxBrethren").find('.row:last-child label:contains("{=id=}")').text(i+".คำนำหน้าชื่อ");
-				// 	$("#boxBrethren .row:last-child").find(".Brethren").each(function(){
-				// 		var idReplace=this.id.replace("{=id=}",i);
-				// 		console.log(idReplace);
-				// 		$(this).attr('id',idReplace);
-				// 	});
-				// }
+			if(this.value > 1 ){
 				for(var i=2;i<=this.value;i++){
-					$("#boxBrethren").find('.row:first-child' ).clone().appendTo("#boxBrethren");
-					$("#boxBrethren").find('.row:last-child label:contains("{=id=}")').text(i+".คำนำหน้าชื่อ");
+					$("#boxBrethren").find('.row:last-child' ).clone().appendTo("#boxBrethren");
+					$("#boxBrethren .row:last-child").find(".No").text(i);
 					$("#boxBrethren .row:last-child").find(".Brethren").each(function(){
-						var idReplace=this.id.replace("{=id=}",i);
-						console.log(idReplace);
-						$(this).attr('id',idReplace);
+						var idExp=this.id.split("_");
+						$(this).attr('id',idExp[0]+'_'+i);
 					});
 				}
-								$("#boxBrethren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
-								$("#boxBrethren .row:first-child").find(".Brethren").each(function(){
-									var idReplace=this.id.replace("{=id=}",1);
-									console.log(idReplace);
-									$(this).attr('id',idReplace);
-								});
 			}
-					if(this.value == 1){
-					$("#boxBrethren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
-					$("#boxBrethren .row:first-child").find(".Brethren").each(function(){
-						var idReplace=this.id.replace("{=id=}",1);
-						console.log(idReplace);
-						$(this).attr('id',idReplace);
-					});
-				}
 		}
 	}).on("change",".BrethrenStatus",function(){
 		var idBox=this.id.split("_");
