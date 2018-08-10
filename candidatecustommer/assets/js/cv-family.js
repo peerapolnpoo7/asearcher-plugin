f0907e7da7b8bfcc25f60cf3194648e0c44f54f0
amornpic <41936114+amornpic@users.noreply.github.com>
2018-08-10 15:17:34 +0700

    2

--- candidatecustommer/assets/js/cv-family.js
+++ candidatecustommer/assets/js/cv-family.js
@@ -30,11 +30,12 @@ $(document).ready(function(){
 		}else{
 			$("#boxChildren").find('.row:first-child').removeClass('hidden');
 			if(this.value > 1){
+				$("#boxChildren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
 				for(var i=2;i<=this.value;i++){
 					$("#boxChildren").find('.row:last-child').clone().appendTo("#boxChildren");
 					$("#boxChildren").find('.row:last-child label:contains("{=id=}")').text(i+".คำนำหน้าชื่อ");
 				}
-				$("#boxChildren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
+				// $("#boxChildren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
 			}
 		}
 	}).on("keyup","#NumBrethren",function(){
@@ -43,21 +44,51 @@ $(document).ready(function(){
 			$("#boxBrethren").find('.row:first-child').addClass('hidden');
 		}else{
 			$("#boxBrethren").find('.row:first-child').removeClass('hidden');
-			if(this.value > 1){
-				for(var i=2;i<=this.value;i++){
-					$("#boxBrethren").find('.row:first-child').clone().appendTo("#boxBrethren");
+			if(this.value >= 1){
+
+
+				for(var i=1;i<this.value;i++){
+					console.log(i);
+					$("#boxBrethren").find('.row:last-child' ).clone().appendTo("#boxBrethren");
 					$("#boxBrethren").find('.row:last-child label:contains("{=id=}")').text(i+".คำนำหน้าชื่อ");
-					$("#boxBrethren .row:last-child").find(".Brethren").each(function(){
-						var idReplace=this.id.replace("{=id=}",i);
-						$(this).attr('id',idReplace);
-					});
+					// $("#boxBrethren .row:last-child").find(".Brethren").each(function(){
+					// 	var idReplace=this.id.replace("{=id=}",i);
+					// 	console.log(idReplace);
+					// 	$(this).attr('id',idReplace);
+					// });
 				}
-				$("#boxBrethren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
-				$("#boxBrethren .row:first-child").find(".Brethren").each(function(){
-					var idReplace=this.id.replace("{=id=}",1);
-					$(this).attr('id',idReplace);
-				});
+
+
+
+
+
+
+
+
+				// for(var i=2;i<=this.value;i++){
+				// 	$("#boxBrethren").find('.row:first-child' ).clone().appendTo("#boxBrethren");
+				// 	$("#boxBrethren").find('.row:last-child label:contains("{=id=}")').text(i+".คำนำหน้าชื่อ");
+				// 	$("#boxBrethren .row:last-child").find(".Brethren").each(function(){
+				// 		var idReplace=this.id.replace("{=id=}",i);
+				// 		console.log(idReplace);
+				// 		$(this).attr('id',idReplace);
+				// 	});
+				// }
+								// $("#boxBrethren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
+								// $("#boxBrethren .row:first-child").find(".Brethren").each(function(){
+								// 	var idReplace=this.id.replace("{=id=}",1);
+								// 	console.log(idReplace);
+								// 	$(this).attr('id',idReplace);
+								// });
 			}
+				// 	if(this.value == 1){
+				// 	$("#boxBrethren").find('.row:first-child label:contains("{=id=}")').text(1+".คำนำหน้าชื่อ");
+				// 	$("#boxBrethren .row:first-child").find(".Brethren").each(function(){
+				// 		var idReplace=this.id.replace("{=id=}",1);
+				// 		console.log(idReplace);
+				// 		$(this).attr('id',idReplace);
+				// 	});
+				// }
 		}
 	}).on("change",".BrethrenStatus",function(){
 		var idBox=this.id.split("_");
