72da3fc5a3803b7f4317e6a42e1838a0dfb849c6
Ol2iGiN <pongthep.dk@gmail.com>
2018-08-15 18:20:12 +0700

    commit

--- candidatecustommer/assets/js/cv-smart.js
+++ candidatecustommer/assets/js/cv-smart.js
@@ -350,7 +350,7 @@ $(document).ready(function(){
         });
         $("input[name='ionrangeSkillListLevel']").val('0');
     }).on("change","#idInstitute_Detail",function(){
-        /*$.request('onGetEducationLevel', {
+        $.request('onGetEducationLevel', {
             data: {value: this.value},
              success: function(data) {
                 $('#idEducation_Level').children('option:not(:first)').remove();
@@ -362,7 +362,7 @@ $(document).ready(function(){
                 });
                 $('select.chosen').trigger("chosen:updated");
              }
-        });*/
+        });
         $.request('onGetFaculty', {
             data: {value: this.value},
              success: function(data) {
@@ -392,17 +392,7 @@ $(document).ready(function(){
              }
          });
     }).on('change','#idEducation_Level',function(){
-        if(this.value==1){
-            $('#boxidGeography').addClass('hidden');
-            $('#boxtype_of_institue').addClass('hidden');
-            $('#boxidInstitute_Detail').addClass('hidden');
-        }else{
-            $('#boxidGeography').removeClass('hidden');
-            $('#boxtype_of_institue').removeClass('hidden');
-            $('#boxidInstitute_Detail').removeClass('hidden');
-            $('#idGeography').val('');
-        }
-        /*$.request('onGetDegreeAndCertificate', {
+        $.request('onGetDegreeAndCertificate', {
             data: {value: this.value},
             success: function(data) {
                 $('#idDegree_and_Certificate').children('option:not(:first)').remove();
@@ -414,7 +404,7 @@ $(document).ready(function(){
                 });
                 $('select.chosen').trigger("chosen:updated");
             }
-        });*/
+        });
     }).on('change','#idFaculty_Detail',function(){
         $.request('onGetDepartment', {
             data: {value: this.value},
@@ -559,7 +549,7 @@ $(document).ready(function(){
     });
     $('#tempidInstitute_Detail').each(function(){
         if(this.value!=""){
-            /*$.request('onGetEducationLevel', {
+            $.request('onGetEducationLevel', {
                 data: {value: this.value},
                  success: function(data) {
                     $('#idEducation_Level').children('option:not(:first)').remove();
@@ -572,8 +562,8 @@ $(document).ready(function(){
                     $("#idEducation_Level").val($("#tempidEducation_Level").val());
                     $('select.chosen').trigger("chosen:updated");
                  }
-            });*/
-            /*$.request('onGetFaculty', {
+            });
+            $.request('onGetFaculty', {
                 data: {value: this.value},
                  success: function(data) {
                     $('#idFaculty_Detail').children('option:not(:first)').remove();
@@ -591,12 +581,12 @@ $(document).ready(function(){
                     }
                     $('select.chosen').trigger("chosen:updated");
                 }
-            });*/
+            });
         }
     });
 
     $("#tempidFaculty_Detail").each(function(){
-        /*$.request('onGetDepartment', {
+        $.request('onGetDepartment', {
             data: {value: this.value},
             success: function(data) {
                 $('#idDepartment').children('option:not(:first)').remove();
@@ -614,7 +604,7 @@ $(document).ready(function(){
                 }
                 $('select.chosen').trigger("chosen:updated");
             }
-        });*/
+        });
     });
 
     $("#tempidDepartment").each(function(){
