aa8997ffa3ad8c45683ab70b8a418dfa4baec62f
Ol2iGiN <pongthep.dk@gmail.com>
2018-08-29 09:13:19 +0700

    commit

--- candidatecustommer/components/ProfileSession.php
+++ candidatecustommer/components/ProfileSession.php
@@ -26,10 +26,11 @@ class ProfileSession extends ComponentBase
 
     public function loadProfiles()
     {
-        $get=Candidate::select('FirstName_TH','LastName_TH','thumb','job_title.Name_TH as JobTitle')
+        $get=Candidate::select('FirstName_TH','LastName_TH','thumb','job_title.Name_TH as JobTitle','job_seeking_status.idJob_Seeking_Status','job_seeking_status.Name_TH as JobSeekStatus')
         ->join('photo_profile_cv','candidate.idCandidate','=','photo_profile_cv.idCandidate')
         ->join('requirement_of_work','candidate.idCandidate','=','requirement_of_work.idCandidate')
         ->join('job_title','requirement_of_work.idJob_Title','=','job_title.idJob_Title')
+        ->join('job_seeking_status','requirement_of_work.idJob_Seeking_Status','=','job_seeking_status.idJob_Seeking_Status')
         ->where('candidate.idCandidate',Session::get('idCandidate'))->first();
         return $get;
 
