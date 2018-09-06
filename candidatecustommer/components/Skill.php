6fdceb641efc8717090541d10c4b16251f7dfdae
Ol2iGiN <pongthep.dk@gmail.com>
2018-09-06 09:34:24 +0700

    commit

--- candidatecustommer/components/Skill.php
+++ candidatecustommer/components/Skill.php
@@ -1,6 +1,8 @@
 <?php namespace Asearcher\Candidatecustommer\Components;
-
+use Auth;
 use Cms\Classes\ComponentBase;
+use Asearcher\CandidateCustommer\Models\SkillList;
+use Asearcher\CandidateCustommer\Models\RequirementOfWork;
 
 class Skill extends ComponentBase
 {
@@ -20,5 +22,16 @@ class Skill extends ComponentBase
      public function onRun(){
         $this->addJs('assets/js/cv-skill.js');
         $this->addCss('assets/css/cv-skill.css');
+        $this->skillLists=$this->loadSkillList();
     }
+
+    public function loadSkillList()
+    {
+        $getRequireOfWork = RequirementOfWork::where('idUser',Auth::getUser()->id)->first();
+        if($getRequireOfWork){
+            $get=SkillList::where('idJob_Category','like','%#'.$getRequireOfWork->idJob_Category.'#%')->skip(0)->take(15)->get()->toArray();
+            return $get;
+        }
+    }
+    public $skillLists;
 }
